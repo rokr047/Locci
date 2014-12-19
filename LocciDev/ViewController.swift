e0b1d87d5a64eb71d52c12728d3c46a2fda3b4d9
Karthikeyan Sreenivasan <rokr047@gmail.com>
2014-12-18 17:02:23 -0800

    Added fade in animations to labels and button.

--- LocciDev/ViewController.swift
+++ LocciDev/ViewController.swift
@@ -12,12 +12,40 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var btnContinue: UIButton!
     
+    @IBOutlet weak var lblOne: UILabel!
+    @IBOutlet weak var lblTwo: UILabel!
+    @IBOutlet weak var lblThree: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Make all description labels hidden.
+        lblOne.alpha = 0.0
+        lblTwo.alpha = 0.0
+        lblThree.alpha = 0.0
+        
+        btnContinue.alpha = 0.0
         // Making the button rounded at the corners
         btnContinue.layer.cornerRadius = 15.0
+        
+        UIView.animateWithDuration(2.0, animations: {
+            self.lblOne.alpha = 1.0
+            }, completion: { (valueOne: Bool) in
+                UIView.animateWithDuration(2.0, animations: {
+                    self.lblTwo.alpha = 1.0
+                    }, completion: { (valueTwo: Bool) in
+                        UIView.animateWithDuration(2.0, animations: {
+                            self.lblThree.alpha = 1.0
+                            }, completion: { (valueThree : Bool) in
+                                UIView.animateWithDuration(2.0, animations: {
+                                    self.btnContinue.alpha = 1.0
+                                }, completion: nil)
+                        })
+                })
+        })
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
