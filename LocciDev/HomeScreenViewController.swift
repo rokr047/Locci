e618e64d6b8d2a9cade2fabe7145ff067c9d6061
Karthikeyan Sreenivasan <rokr047@gmail.com>
2014-12-26 23:10:56 -0800

    - Test Data with TableViewController inside ViewController [Home]

--- LocciDev/HomeScreenViewController.swift
+++ LocciDev/HomeScreenViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 import MapKit
 
-class HomeScreenViewController: UIViewController, CLLocationManagerDelegate {
+class HomeScreenViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
 
     var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var notesTable: UITableView!
+    
+    //TableView Test
+    var testData: Array<AnyObject> = []
+    var testDetailData: Array<AnyObject> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,11 @@ class HomeScreenViewController: UIViewController, CLLocationManagerDelegate {
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
+        
+        //TableView Test
+        testData = ["Apple", "Google", "Facebook", "Microsoft", "Dropbox", "Twitter", "Electronic Arts", "Rockstar", "Ubisoft", "Zynga", "Scopely", "Imangi", "Backflip", "Valve", "Amazon", "Flipkart"]
+        
+        testDetailData = ["Apple", "Google", "Facebook", "Microsoft", "Dropbox", "Twitter", "Electronic Arts", "Rockstar", "Ubisoft", "Zynga", "Scopely", "Imangi", "Backflip", "Valve", "Amazon", "Flipkart"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,5 +39,26 @@ class HomeScreenViewController: UIViewController, CLLocationManagerDelegate {
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return testData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("noteCell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = testData[indexPath.row] as? String
+        cell.detailTextLabel?.text = testData[indexPath.row] as? String
+        
+        return cell
     }
 }
